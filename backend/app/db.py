05ca10f6661efe1e6796dab38cd8b069067027ff
Andrea <andrea.ferretti1998@gmail.com>
2026-05-19 14:37:59 +0200

    Update

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,13 +1,17 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
-engine = create_async_engine(os.environ["DATABASE_URL"], echo=False)
+# Prende la stringa asincrona dal .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
+# Questa è la classe che mancava e che serve ad Alembic!
 class Base(DeclarativeBase):
     pass
 
